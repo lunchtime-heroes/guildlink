173a9a435ca93b9b2418629a10d5597c6731bed5
Matt <matt@lunchtimeheroes.co>
2026-04-03 21:19:43 -0400

    updated supabase js

--- src/supabase.js
+++ src/supabase.js
@@ -2,7 +2,8 @@ import { createClient } from "@supabase/supabase-js";
 
 const supabase = createClient(
   "https://zpalkpcqihxamedymnwe.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpwYWxrcGNxaWh4YW1lZHltbndlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI4NDc3MTQsImV4cCI6MjA4ODQyMzcxNH0.8V9MEXpcCH8dibm65PVtaPZseDbPvYCwSPJQ-9Cu-Zo"
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpwYWxrcGNxaWh4YW1lZHltbndlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI4NDc3MTQsImV4cCI6MjA4ODQyMzcxNH0.8V9MEXpcCH8dibm65PVtaPZseDbPvYCwSPJQ-9Cu-Zo",
+  { auth: { flowType: "implicit" } }
 );
 
 export default supabase;
